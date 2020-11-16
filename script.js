e131fe92f119f7a86d82fe88fbf51d8c1623f0cb
Maja Gedda <maja.gedda@zocom.se>
2020-11-16 14:08:13 +0100

    heh

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let dogSpace = document.getElementById('dog');
 
 const url = 'https://api.jsonbin.io/b/5f4d604b514ec5112d136cd6';
 
+let getDog = () => {
+    fetch(url)
+    .then(response => response.json())
+    .then(response => console.log(response))
+};
+
+getDog();
