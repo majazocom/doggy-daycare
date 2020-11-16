fedc623544971e757bbea373399c6c26ad18d7f1
Maja Gedda <maja.gedda@zocom.se>
2020-11-16 15:08:08 +0100

    More

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let btn = document.getElementById('btn');
-let dogSpace = document.getElementById('dog');
+const btn = document.getElementById('btn');
+const dogSpace = document.getElementById('dog');
+const dogAge = document.getElementById('age');
 
 const url = 'https://api.jsonbin.io/b/5f4d604b514ec5112d136cd6';
 
-let getDog = () => {
+let dogs;
+
+let getDogs = () => {
     fetch(url)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => {
+        dogs = response
+        getRandomDog();
+    })
 };
 
-getDog();
+let getRandomDog = () => {
+    let dog = dogs[Math.floor(Math.random() * dogs.length)];
+    dogSpace.innerHTML = `<img src="${dog.img}"><br><p>${dog.name}</p>`;
+    dogAge.innerHTML = `<p>${dog.age}</p>`;
+}
+
+btn.addEventListener('click', () => {
+    getDogs();
+});
